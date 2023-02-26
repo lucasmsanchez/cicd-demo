7b791a72e79c54ec19edd36d785331567abdeb40
Lucas <lucasmsanchez@protonmail.com>
2023-02-25 21:52:20 -0800

    Teaching demonstration files

---
+++ my-app/src/main/java/teaching/Demonstration.java
@@ -0,0 +1,29 @@
+package teaching;
+
+public class Demonstration {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+	
+	public static void operatorsGo() {
+		
+	}
+	
+	public static void arrayCrudGo() {
+		
+	}
+	
+	public static void expressionsGo() {
+		
+	}
+	
+	public static void controlFlowGo() {
+		
+	}
+	
+	public static void switchingGo() {
+		
+	}
+
+}
---
+++ my-app/src/main/java/teaching/OctoCat.java
@@ -0,0 +1,26 @@
+package teaching;
+
+public class OctoCat {
+	
+	// Primitive types
+	static byte byteType = 127;
+	static short shortType = 32767;
+	static int intType = 2^31 - 1;  
+	static long creditCardNumber = 1234_5678_9012_3456L; 
+	static float floatType = (float) 1.337;
+	static double doubleType = (double) 4.222;
+	static boolean reifiedBeing = true;
+	static char charType = 'A';
+	
+	// Complex types
+	static String immutableString = "Knox";
+	static int[] arrayOfIntegers = {1, 2, 3, 4, 5};
+	static String[] arrayOfStrings = {"Eeny", "Meeny", "Miney", "Mo"};
+	
+	// instance specific 
+	public String name;
+	public int age;
+	public long legs;
+	public long eyes;
+	
+}
